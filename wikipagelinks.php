e5c9298275188193cf8c035f15a8bd15948ab407
Darcy Casselman <Darcy Casselman@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-06-27 01:36:44 +0000

    fix type-o
    git-svn-id: https://plugins.svn.wordpress.org/wiki-page-links/trunk@401541 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wikipagelinks.php
+++ wikipagelinks.php
@@ -119,9 +119,10 @@ class WikiLinksPlugin {
 				$content = str_replace($match, 
 					"<a href='". get_permalink($page->ID) ."'>$page_title</a>",
 					$content);
-			} else if ( $is_user_logged_in() ) {{
+			} else if ( $is_user_logged_in() ) {
 				//Add a link to create the page if it doesn't exist.
 				//TODO: limit showing the link to users who can create posts.
+
 				$home = get_option('siteurl');
 				$encodedlink = urlencode($link);
 				$content = str_replace($match, "{$page_title}[<a href='$home/wp-admin/post-new.php?post_type=page&post_title=$encodedlink' class='nonexistant_page' title='Create this page (requires a valid \"contributer\" account)'>?</a>]", $content);
